76e8151060844a10c775510455b57c3b9eb56fe2
Your Name <you@example.com>
2024-05-09 19:17:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -76,6 +76,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
                     tree->current = aux->right;
                     return;
                 }
+                else aux = aux->right;
             }
             
         }
@@ -91,8 +92,60 @@ TreeNode * minimum(TreeNode * x)
 }
 
 
-void removeNode(TreeMap * tree, TreeNode* node) {
-
+void removeNode(TreeMap * tree, TreeNode* node) 
+{
+    if(tree == NULL || tree->root == NULL || node == NULL) return;
+    if(node->left == NULL && node->right == NULL)
+    {
+        if(node->parent->left == node) node->parent->left = NULL;
+        {
+            if(node->parent->right == node) node->parent->right = NULL;
+            {
+                free(node->pair->key);
+                free(node->pair->value);
+                free(node->pair);
+                free(node);
+                return;
+            }
+        }
+    }
+    if(node->left != NULL && node->right == NULL)
+    {
+        if(node->parent->left == node) node->parent->left = node->left;
+        {
+            if(node->parent->right == node) node->parent->right = node->left;
+            {
+                free(node->pair->key);
+                free(node->pair->value);
+                free(node->pair);
+                free(node);
+                return;
+            }
+            
+        }
+        
+    }
+    if(node->left == NULL && node->right != NULL)
+    {
+        if(node->parent->left == node) node->parent->left = node->right;
+        {
+            if(node->parent->right == node) node->parent->right = node->right;
+            {
+                free(node->pair->key);
+                free(node->pair->value);
+                free(node->pair);
+                free(node);
+                return;
+            }
+        }
+    }
+    if(node->left != NULL && node->right != NULL)
+    {
+        TreeNode * min = minimum(node->right);
+        node->pair->key = min->pair->key;
+        node->pair->value = min->pair->value;
+        removeNode(tree, min);
+    }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
